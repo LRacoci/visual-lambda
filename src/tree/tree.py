b5bbfe28886be9c70901a6e5d9c762f0b0344ddd
Jose Pedro Vieira do Nascimento Filho <j.pedronascimentofilho@gmail.com>
2018-04-10 23:11:16 -0300

    Show content in textarea

--- src/tree/tree.py
+++ src/tree/tree.py
@@ -1,44 +1,47 @@
 def leaves(tree, k=0):
-    if type(tree) is list:
-        for branch in tree:
-            for leaf, j in leaves(branch, k=k+1):
-                yield leaf, j
-    else:
-        yield tree, k
+	if type(tree) is list:
+		for branch in tree:
+			for leaf, j in leaves(branch, k=k+1):
+				yield leaf, j
+	else:
+		yield tree, k
+
+def change(forestDicts):
+	forest = forestDicts["functions"]
+	dependence = {}
+	for root in forest:
+		dependence[root] = {}
+		for leaf,_ in leaves(forest[root]):
+			if leaf in forest:
+				dependence[root][leaf] = root
+	print dependence
+	def aux(tree, k = 0):
+		if type(tree) is list:
+			children = []
+			for branch in tree:
+				children += [aux(branch, k=k+1)]
+			return {
+				"name": " ",
+				"children": children
+			}
+		else:
+			return {"name": str(tree)}
+	return {root: aux(forest[root]) for root in forest}
 
 
-def change(forest):
-    dependence = {}
-    for root in forest:
-        for leaf,_ in leaves(forest[root]):
-            if leaf in forest:
-                dependence[leaf] = root
-    print dependence
-    def aux(tree, k = 0):
-        if type(tree) is list:
-            children = []
-            for branch in tree:
-                children += [aux(branch, k=k+1)]
-            return {
-                "name": " ",
-                "children": children
-            }
-        else:
-            return {"name": str(tree)}
-    return {root: aux(forest[root]) for root in forest}
-    
-    
 if __name__ == "__main__":
-    from glob import glob
-    import json
-    for fileAddress in glob('tests/arq*.json'):
-        print fileAddress
-        
-        with open(fileAddress) as jsonInputFile:
-            jsonInput = json.load(jsonInputFile)
-        
-        jsonOutput = change(jsonInput)
-        print jsonOutput
-        
-        with open(fileAddress.replace("arq","forest"), "w") as outputFile:
-            json.dump(jsonOutput, outputFile, indent=1)
+	from glob import glob
+	import json
+	for fileAddress in glob('tests/arq7.json'):
+		print fileAddress
+
+		with open(fileAddress) as jsonInputFile:
+			jsonInput = json.load(jsonInputFile)
+		print "Jason"
+		print jsonInput
+		print "End"
+		jsonOutput = change(jsonInput)
+		print jsonOutput
+
+		with open(fileAddress.replace("arq","forest"), "w") as outputFile:
+			json.dump(jsonOutput, outputFile, indent=1)
