9a4fbee02f81df9b21ccd4af739b9cf27e9e47d6
Luizfrf3 <lf.fonseca@hotmail.com>
2018-06-10 02:09:47 -0300

    Header with pattern matching

--- src/lexer_parser/header.hs
+++ src/lexer_parser/header.hs
@@ -1,44 +1,37 @@
 -- Header
-map f list =
-  if list == []
-    then list
-    else [f(list[0])] + map(f)(rest(list))
+map f [] = []
+map f list = [f(list[0])] + map(f)(rest(list))
 
-filter f list =
-  if list == []
-    then []
-    else (
+filter f [] = []
+filter f list = (
       if f(list[0])
         then [list[0]]
         else []
       ) + filter(f)(rest(list))
 
-fold func list initial =
-  if list == []
-    then initial
-    else func(list[0])(
+fold func [] initial = initial
+fold func list initial = func(list[0])(
       fold
         (func)
         (rest(list))
         (initial)
     )
 
-rest list = if list == []
-  then []
-  else end(list)(1)
+rest [] = []
+rest list = end(list)(1)
 
 end list x = if list[x] == None
   then []
   else [list[x]] + end(list)(x+1)
 
-first list = if list == [] then [] else list[0]
+first [] = []
+first list = list[0]
 
 init list n = if list[n] == None
   then []
   else init(list)(n-1) + [list[n]]
 
-len ls = if ls == []
-  then 0
-  else 1 + len(rest(ls))
+len [] = 0
+len ls = 1 + len(rest(ls))
 
 last ls = ls[len(ls)-1]
